5fdd2d3455f693517768e497ba8873bf04bf2b89
Steven Loaiza <steven.loaiza@outlook.com>
2025-01-18 13:54:46 -0500

    Add CausalAttention module

--- libs/llm_builder/components/layers.py
+++ libs/llm_builder/components/layers.py
@@ -44,3 +44,53 @@ class SelfAttention(nn.Module):
         context_vector = attn_weights @ value
 
         return context_vector
+
+
+class CausalAttention(SelfAttention):
+    """
+    Causal Attention Layer for Transformer Models (PyTorch)
+    This class implements a causal attention mechanism, which is a core component of transformer models. 
+    It computes attention scores between input sequences to capture dependencies and relationships, while preventing
+    information leakage from future tokens. Additionally, it includes a dropout layer to regularize the attention weights.
+    Attributes:
+        d_in (int): Dimensionality of the input features.
+        d_out (int): Dimensionality of the output features.
+        context_length (int): Length of the context for the attention mechanism.
+        dropout (float): Dropout rate applied to the attention weights.
+        qkv_bias (bool): Whether to include a bias term in the linear projections for query, key, and value.
+    Methods:
+        forward(x):
+            Computes the causal attention mechanism on the input tensor `x`.
+            Args:
+                x (torch.Tensor): Input tensor of shape (batch_size, seq_length, d_in).
+            Returns:
+                torch.Tensor: Output tensor of shape (batch_size, seq_length, d_out) after applying causal attention.
+    """
+    
+    def __init__(self, d_in: int, d_out: int, context_length:int, dropout:float, qkv_bias: bool = True):
+        super().__init__(d_in, d_out, qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        # Matrix of diagnoal 1s, to be used as a mask
+        # registered as a buffer to be moved to the device with the model
+        # this is a constant tensor that does not require gradients
+        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
+        
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        key = self.W_key(x)
+        query = self.W_query(x)
+        value = self.W_value(x)
+
+        # Transpose dimentions 1 and 2, keeping the batch dimension at the first index
+        attn_score = query @ key.transpose(1,2)
+        # Apply softmax to "-inf" values results in 0 and applies a mask to the future tokens
+        # In Pytorch, operations with trailing underscores modify the tensor in-place
+        attn_score = attn_score.masked_fill_(
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(
+            attn_score / key.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights)
+
+        context_vector = attn_weights @ value
+        return context_vector
--- libs/test/test_attention.py
+++ libs/test/test_attention.py
@@ -4,10 +4,17 @@ import sys
 sys.path.insert(0, str(Path(__file__).parents[2]))
 
 import torch
-from libs.llm_builder.components.layers import SelfAttention
+from libs.llm_builder.components.layers import SelfAttention, CausalAttention
 
-def test_attentionv1():
+def test_attention():
     attention = SelfAttention(d_in=2, d_out=2)
     output = attention.forward(x=torch.tensor([[1., 2.], [2., 3.], [3., 4.]]))
     assert output.shape == (3, 2)
-    
+
+def test_causal_attention():
+    x=torch.tensor([[1., 2.], [2., 3.], [3., 4.]])
+    x_batch = torch.stack([x,x], dim=0)
+
+    causal_attention = CausalAttention(d_in=2, d_out=2, context_length=x_batch.shape[1], dropout=0.1)
+    output = causal_attention.forward(x=x_batch)
+    assert output.shape[0] == 2 # batch size
